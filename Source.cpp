aae99b93af311f9df3fa27d083a2c05a70743169
Senichkaa <90269692+Senichkaa@users.noreply.github.com>
2021-10-13 11:55:55 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+// Lab_04_1.cpp
+// < Капанайко Арсен >
+// Лабораторна робота № 4.1
+// Цикли.
+// Варіант 9.1
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int  i;
+	double S;
+	//cout << "i = "; cin >> i;
+	//cout << "l = "; cin >> l;
+	S = 0;
+	i = 1;
+	while (i <= 15)
+	{
+		S += (sin(10. * i) + cos(10. / i)) / sqrt(i);
+		i++;
+	}
+	cout << S << endl;
+	S = 0;
+	i = 1;
+	do {
+		S += (sin(10. * i) + cos(10. / i)) / sqrt(i);
+		i++;
+	} while (i <= 15);
+	cout << S << endl;
+	S = 0;
+	for (i = 1; i <= 15; i++)
+	{
+		S += (sin(10. * i) + cos(10. / i)) / sqrt(i);
+	}
+	cout << S << endl;
+	S = 0;
+	for (i = 15; i >= 1; i--)
+	{
+		S += (sin(10. * i) + cos(10. / i)) / sqrt(i);
+	}
+	cout << S << endl;
+	return 0;
+}
